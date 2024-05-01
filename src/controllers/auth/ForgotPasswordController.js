c1a6ca2ad69392f5284e15eee0790dbe36fa3a1f
Your NameRafael <rafaaquino28@gmail.com>
2024-04-30 23:41:48 -0300

    feat: atualizacao de retorno

--- src/controllers/auth/ForgotPasswordController.js
+++ src/controllers/auth/ForgotPasswordController.js
@@ -54,7 +54,9 @@ module.exports = class ForgotPasswordController {
       if (error) {
         res.status(500).json({ message: "Error sending email", error });
       } else {
-        res.status(200).json({ message: "Email successfully sent" });
+        res
+          .status(200)
+          .json({ message: "Email successfully sent", email: email });
       }
     });
   }
