64d9814f29e7796475b3c7431b1579ba84f322f7
Your NameRafael <rafaaquino28@gmail.com>
2024-04-30 23:41:06 -0300

    feat: att campo de busca para codigo de autenticacao

--- src/controllers/auth/CodePasswordController.js
+++ src/controllers/auth/CodePasswordController.js
@@ -3,7 +3,7 @@ const createUserToken = require("../../helpers/create-user-token");
 
 module.exports = class CodePasswordController {
   static async codeAuthPassword(req, res) {
-    const { code } = req.body;
+    const { code, email } = req.body;
 
     console.log(code);
 
@@ -11,24 +11,33 @@ module.exports = class CodePasswordController {
       return res.status(422).json({ message: "Code is required" });
     }
 
-    const user = await User.findOne({ passwordResetToken: code }).select(
-      "+passwordResetToken passwordResetExp"
-    );
-
-    console.log("req: ", req.body);
-    console.log("userExists: ", user);
-
-    if (code !== user.passwordResetToken) {
-      return res.status(400).json({ error: "Invalid code" });
-    }
-
-    const now = new Date();
-
-    if (now > user.passwordResetExp) {
-      return res.status(400).json({ error: "Code expire, generate a new one" });
-    }
-    console.log("user antigo: ", user);
     try {
+      const user = await User.findOne({ email: email }).select(
+        "+passwordResetToken passwordResetExp"
+      );
+
+      if (user == null) {
+        return res.status(401).json({
+          message: "Invalid code! Enter the correct code or generate a new one",
+        });
+      }
+
+      console.log("req: ", req.body);
+      console.log("userExists: ", user);
+
+      if (code !== user.passwordResetToken) {
+        return res.status(400).json({ error: "Invalid code" });
+      }
+
+      const now = new Date();
+
+      if (now > user.passwordResetExp) {
+        return res
+          .status(400)
+          .json({ error: "Code expire, generate a new one" });
+      }
+      console.log("user antigo: ", user);
+
       await createUserToken(user, req, res);
       await User.findOneAndUpdate(
         { passwordResetToken: code }, // Condição de busca
@@ -36,7 +45,9 @@ module.exports = class CodePasswordController {
         { new: true } // Retorna o documento atualizado
       ).select("+passwordResetToken passwordResetExp"); //
     } catch (error) {
-      res.status(500).json({ message: "Cannot reset password", error: error });
+      res
+        .status(500)
+        .json({ message: "Cannot reset password", error: error.message });
     }
   }
 };
