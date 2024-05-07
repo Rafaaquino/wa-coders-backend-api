45176017131138634b843213c86ae0f0011695b3
Your NameRafael <rafaaquino28@gmail.com>
2024-05-07 12:03:47 -0300

    feat: conexao com banco

--- src/db/connect-db.js
+++ src/db/connect-db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const config = require("../app/config/config");
 
 async function main() {
-  await mongoose.connect(config.MONGODB_URI);
+  await mongoose.connect(
+    "mongodb+srv://wacoders:gDU7Jp7hNPZmYGyw@wacodersdb.et0tbe3.mongodb.net/?retryWrites=true&w=majority&appName=wacodersdb"
+  );
   console.log(`conectou ao banco!`);
 }
 
