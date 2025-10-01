a56fa880d671ccd4dc3614b2769e52b95268f8be
Your NameRafael <rafaaquino28@gmail.com>
2025-10-01 19:49:58 -0300

    db

--- src/db/connect-db.js
+++ src/db/connect-db.js
@@ -1,23 +1,23 @@
 // src/db/db.js
 const mongoose = require("mongoose");
-const config = require("../app/config/config");
 
-if (!config.MONGODB_URI) {
+if (!process.env.MONGO_URI) {
   console.error(
-    "❌ MONGODB_URI não definido. Configure a variável de ambiente no Vercel."
+    "❌ MONGO_URI não definido. Configure a variável de ambiente no Vercel."
   );
-  process.exit(1); // sai do processo para não continuar com URI indefinida
+  process.exit(1); // sai do processo
 }
 
 async function connectDB() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://wacoders:gDU7Jp7hNPZmYGyw@wacodersdb.et0tbe3.mongodb.net/?retryWrites=true&w=majority&appName=wacodersdb"
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ Conectou ao banco MongoDB!");
   } catch (err) {
     console.error("❌ Erro ao conectar no MongoDB:", err);
-    process.exit(1); // encerra a aplicação se não conseguir conectar
+    process.exit(1);
   }
 }
 
