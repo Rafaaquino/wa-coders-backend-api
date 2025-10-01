2c9b3769af08c20721875f074dddecd4050ea085
Your NameRafael <rafaaquino28@gmail.com>
2025-10-01 19:59:02 -0300

    db

--- src/db/connect-db.js
+++ src/db/connect-db.js
@@ -3,17 +3,14 @@ const mongoose = require("mongoose");
 
 if (!process.env.MONGODB_URI) {
   console.error(
-    "❌ MONGO_URI não definido. Configure a variável de ambiente no Vercel."
+    "❌ MONGODB_URI não definido. Configure a variável de ambiente no Vercel."
   );
   process.exit(1); // sai do processo
 }
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("✅ Conectou ao banco MongoDB!");
   } catch (err) {
     console.error("❌ Erro ao conectar no MongoDB:", err);
